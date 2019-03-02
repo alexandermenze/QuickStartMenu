4a46d810b26df50a4d4181be5c9b8e847894c192
Alexander Menze <a.menze@polipol.de>
2019-03-02 09:57:03 +0100

    Initial project structure

---
+++ QuickStartMenu/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace QuickStartMenu
+{
+    /// <summary>
+    /// Interaktionslogik für "App.xaml"
+    /// </summary>
+    public partial class App : Application
+    {
+        private void App_OnStartup(object sender, StartupEventArgs e)
+        {
+            new MenuWindow().Show();
+        }
+    }
+}
---
+++ QuickStartMenu/MenuWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+
+namespace QuickStartMenu
+{
+    /// <summary>
+    /// Interaktionslogik für MenuWindow.xaml
+    /// </summary>
+    public partial class MenuWindow : Window
+    {
+        public MenuWindow()
+        {
+            InitializeComponent();
+            Deactivated += OnDeactivated;
+        }
+
+        private void OnDeactivated(object sender, EventArgs e) 
+            => Close();
+    }
+}
